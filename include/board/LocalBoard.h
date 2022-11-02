02b93d07a67c8c444454615bf513b641b2079b99
Vincent Putzke <putzkvin@b-tu.de>
2022-11-02 14:22:17 +0100

    Reset Board; New LocalBoard Header

---
+++ include/board/LocalBoard.h
@@ -0,0 +1,87 @@
+#ifndef LOCAL_BOARD_H
+#define LOCAL_BOARD_H
+
+#include <sys/types.h>
+#include <vector>
+#include <string>
+#include <Board.h>
+
+class LocalBoard : Board
+{
+    public:
+        /**
+         * Create a board with a given size, where the upper left corner is 0,0
+         *
+         * @param width is the horizontal size of the board
+         * @param height is the vertical size of the board
+         */
+        LocalBoard(size_t width, size_t height) : Board(width, height) {}
+
+        /**
+         * Because this is a pure virtual class, we should declare
+         * the destructor also as virtual.
+         */
+        virtual ~LocalBoard() {}
+
+        /**
+         * Get the liveliness of a specified element.
+         *
+         * @param x is the horizontal position of the element
+         * @param y is the vertical position of the element
+         * @return dead or alive if element exists, else invalid
+         */
+        virtual enum life_status_t getPos(size_t x, size_t y) = 0;
+
+        /**
+         * Sets a element to a life status. If position or status is invalid,
+         * nothing is done.
+         *
+         * @param x is the horizontal position of the element
+         * @param y is the vertical position of the element
+         * @param status is the life status to be set
+         */
+        virtual void setPos(size_t x, size_t y, enum life_status_t status) = 0;
+        /**
+         * Exports this board to output file.
+         *
+         * @return true, if successful
+         * @return false, else
+         */
+        virtual bool exportAll(std::string destFileName) = 0;
+
+        /**
+         * Imports to this board from output file.
+         *
+         * @return true
+         * @return false
+         */
+        virtual bool importAll(std::string sourceFileName) = 0;
+
+        /**
+         * Performs one step on this board.
+         */
+        virtual void step() = 0;
+
+    protected:
+        /**
+         * Sets an element to a life status. Invalid inputs will be discarded.
+         *
+         * @param x is the horizontal position of the element
+         * @param y is the vertical position of the element
+         * @param status is the life status to be set
+         */
+        virtual void setPosRaw(size_t x, size_t y, enum life_status_t status) = 0;
+
+        /**
+         * Gets the life status of an element. Invalid inputs will return invalid.
+         *
+         * @param x is the horizontal position of the element
+         * @param y is the vertical position of the element
+         */
+        virtual enum life_status_t getPosRaw(size_t x, size_t y) = 0;
+
+        // 1-Dimensional representation of the field (y * width + x, to access (x,y))
+        std::vector<enum life_status_t> field;
+};
+
+#endif
--- src/board/LocalBoard.cc
+++ src/board/LocalBoard.cc
@@ -1,85 +1,79 @@
 #include <sys/types.h>
 #include <vector>
 #include <string>
-#include <Board.h>
+#include <LocalBoard.h>
 
-class LocalBoard : Board
+void LocalBoard::setPos(size_t x, size_t y, enum life_status_t status)
 {
-public:
-    LocalBoard::LocalBoard(size_t width, size_t height) : Board(width, height) {}
-    void setPos(size_t x, size_t y, enum life_status_t status)
+    while (x < 0)
     {
-        while (x < 0)
-        {
-            x += width;
-        }
-        while (y < 0)
-        {
-            y += height;
-        }
-        return setPosRaw(x % width, y % height, status);
+        x += width;
     }
-    enum life_status_t getPos(size_t x, size_t y)
+    while (y < 0)
     {
-        while (x < 0)
-        {
-            x += width;
-        }
-        while (y < 0)
-        {
-            y += height;
-        }
-        return getPosRaw(x % width, y % height);
+        y += height;
     }
-    void step()
+    LocalBoard::setPosRaw(x % width, y % height, status);
+}
+enum life_status_t LocalBoard::getPos(size_t x, size_t y)
+{
+    while (x < 0)
     {
-        std::vector<enum life_status_t> newField;
-        for (int x = 0; x < width; x++)
+        x += width;
+    }
+    while (y < 0)
+    {
+        y += height;
+    }
+    return LocalBoard::getPosRaw(x % width, y % height);
+}
+void LocalBoard::step()
+{
+    std::vector<enum life_status_t> newField;
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
         {
-            for (int y = 0; y < height; y++)
+            int neighbourCount = 0;
+            enum life_status_t status = getPos(x, y);
+            for (int dx = -1; dx <= 1; dx++)
             {
-                int neighbourCount = 0;
-                enum life_status_t status = getPos(x, y);
-                for (int dx = -1; dx <= 1; dx++)
+                for (int dy = -1; dy <= 1; dy++)
                 {
-                    for (int dy = -1; dy <= 1; dy++)
-                    {
-                        if(dx != 0 && dy != 0){
-                            if (getPos(x + dx, y + dy) == life_status_t::alive)
-                            {
-                                neighbourCount++;
-                            }
+                    if(dx != 0 && dy != 0){
+                        if (getPos(x + dx, y + dy) == life_status_t::alive)
+                        {
+                            neighbourCount++;
                         }
                     }
                 }
-                if(status == life_status_t::dead && neighbourCount == 3)
-                {
-                    newField.push_back(life_status_t::alive);
-                }
-                else if(status == life_status_t::alive && neighbourCount >= 2 && neighbourCount <= 3)
-                {
-                    newField.push_back(life_status_t::alive);
-                }else{
-                    newField.push_back(life_status_t::dead);
-                }
             }
-        }
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
+            if(status == life_status_t::dead && neighbourCount == 3)
             {
-                setPosRaw(x, y, newField[y * width + x]);
+                newField.push_back(life_status_t::alive);
+            }
+            else if(status == life_status_t::alive && neighbourCount >= 2 && neighbourCount <= 3)
+            {
+                newField.push_back(life_status_t::alive);
+            }else{
+                newField.push_back(life_status_t::dead);
             }
         }
     }
-protected:
-    void setPosRaw(size_t x, size_t y, enum life_status_t status)
+    for (int x = 0; x < width; x++)
     {
-        field[y * width + x] = status;
+        for (int y = 0; y < height; y++)
+        {
+            setPosRaw(x, y, newField[y * width + x]);
+        }
     }
-    enum life_status_t getPosRaw(size_t x, size_t y)
-    {
-        return field[y * width + x];
-    }
+}
 
-};
+void LocalBoard::setPosRaw(size_t x, size_t y, enum life_status_t status)
+{
+    field[y * width + x] = status;
+}
+enum life_status_t LocalBoard::getPosRaw(size_t x, size_t y)
+{
+    return field[y * width + x];
+}
