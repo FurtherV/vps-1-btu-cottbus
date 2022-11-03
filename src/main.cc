fa2506116760767c3ad4c7344d18a90c302efeac
FurtherV <alex.howel@web.de>
2022-11-03 13:37:38 +0100

    add command line usages to main

--- src/main.cc
+++ src/main.cc
@@ -10,18 +10,34 @@
 using namespace std;
 using namespace GUI;
 
+/**
+ * @brief The game of life, with an optional graphical representation.
+ *
+ * @details This program supports the following command line options:
+ * -i <FILE> 	=> Loads the given file as a board configuration.
+ * -o <FILE> 	=> Saves the board configuration (after the simulation) as given file.
+ * -r <NUMBER> 	=> Amount of simulation steps to be done.
+ * -g 			=> If set, a GUI will be used.
+ * -a			=> If set, Board will be drawn to match aspect ratio of window to board.
+ * 				(IE: cells might not be squares).
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char *argv[])
 {
 	int option_val = 0;
 
 	string input_path = "RANDOM";
-	string output_path;
+	string output_path = "";
 
-	int steps = 1;
+	int steps = 0;
 
 	bool graphical = false;
+	bool aspect = false;
 
-	while ((option_val = getopt(argc, argv, ":i:o:r:g")) != -1)
+	while ((option_val = getopt(argc, argv, ":i:o:r:ga")) != -1)
 	{
 		switch (option_val)
 		{
@@ -40,28 +56,57 @@ int main(int argc, char *argv[])
 		case 'g':
 			graphical = true;
 			break;
+		case 'a':
+			aspect = true;
+			break;
 		default:
 			break;
 		}
 	}
 
-	LocalBoard board(100, 100);
+	if (input_path == "RANDOM")
+	{
+		cout << "[INFO] No board configuration specified, GoL will use random configuration." << endl;
+	}
+
+	if (output_path == "")
+	{
+		cout << "[INFO] No output file specified, GoL will not write anything to disk." << endl;
+	}
+
+	LocalBoard board(3, 3);
 	board.importAll(input_path);
 
-	DrawingWindow window(800, 800, "Game of Life");
-	BoardDrawer drawer(&window, &board, false);
-	drawer.draw();
-
-	for (int i = 0; i < 10; i++)
+	if (graphical)
 	{
-		char userInput;
-		cin >> userInput;
-
-		if (userInput == 'q')
-			break;
-		board.step();
+		DrawingWindow window(800, 800, "Game of Life");
+		BoardDrawer drawer(&window, &board, !aspect);
 		drawer.draw();
+
+		cout << "Simulation Step: 0" << endl;
+		cout << "Press enter to continue..." << endl;
+		cin.get();
+		for (int i = 0; i < steps; i++)
+		{
+			board.step();
+			drawer.draw();
+			cout << "Simulation Step: " << i + 1 << endl;
+			cout << "Press enter to continue..." << endl;
+			cin.get();
+		}
 	}
-	std::cout << "sad" << input_path << endl;
+	else
+	{
+		for (int i = 0; i < steps; i++)
+		{
+			board.step();
+		}
+	}
+
+	if (output_path != "")
+	{
+		board.exportAll(output_path);
+	}
+	cout << "Done." << endl;
 	return 0;
 }
