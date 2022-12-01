e307782b4758b2cdf7ecabda74c09ef64522f989
FurtherV <alex.howel@web.de>
2022-12-01 10:01:02 +0100

    fixed mode option

--- src/main_mpi.cc
+++ src/main_mpi.cc
@@ -5,9 +5,12 @@
 #include "client/LifeClientMPISimple.h"
 #include "misc/Log.h"
 #include "misc/Stopwatch.h"
+#include <algorithm>
 #include <boost/program_options.hpp>
+#include <cctype>
 #include <exception>
 #include <mpi.h>
+#include <string>
 
 /**
  * @brief Initial method called by the operating system upon executing this program.
@@ -21,15 +24,16 @@ int main(int argc, char **argv) {
 
     // define available arguments
     po::options_description desc("Usage", 1024, 512);
-    desc.add_options()                                                                                      //
-        ("help,", "Print help message")                                                                     //
-        ("input,i", po::value<string>(), "Input file\nMust be in the correct .rle format")                  //
-        ("output,o", po::value<string>(), "Output file\nExisting files will be overwriten")                 //
-        ("steps,r", po::value<int>()->default_value(1), "Simulation steps")                                 //
-        ("width,w", po::value<int>()->default_value(100), "Width of the board\nNot compatible with -i")     //
-        ("height,h", po::value<int>()->default_value(100), "Height of the board\nNot compatible with -i")   //
-        ("profile,", po::value<string>(), "Output file for profiler")                                       //
-        ("mode,m", po::value<string>()->default_value("simple"), "Client-/Server Mode\nsimple or advanced");//
+    desc.add_options()                                                                                    //
+        ("help,", "Print help message")                                                                   //
+        ("input,i", po::value<string>(), "Input file\nMust be in the correct .rle format")                //
+        ("output,o", po::value<string>(), "Output file\nExisting files will be overwriten")               //
+        ("steps,r", po::value<int>()->default_value(1), "Simulation steps")                               //
+        ("width,w", po::value<int>()->default_value(100), "Width of the board\nNot compatible with -i")   //
+        ("height,h", po::value<int>()->default_value(100), "Height of the board\nNot compatible with -i") //
+        ("profile,", po::value<string>(), "Output file for profiler")                                     //
+        ("mode,m", po::value<string>()->default_value("simple"),
+         "MPI Mode, one of:\n  simple) For homogenous systems\n  advanced) For heterogenous systems"); //
 
     // read arguments and store in a map
     po::variables_map vm;
@@ -39,6 +43,13 @@ int main(int argc, char **argv) {
     } catch (po::error &e) {
         LOG(ERROR) << e.what();
         cerr << desc << endl;
+    }
+
+    // Print help text and exit on --help
+    if (vm.count("help")) {
+        cerr << "Game of Life MPI" << endl;
+        cerr << desc << endl;
+        return 0;
     }
 
     // store arguments as variables
@@ -58,22 +69,23 @@ int main(int argc, char **argv) {
         profiler_output = vm["profile"].as<std::string>();
     }
     string mode = vm["mode"].as<std::string>();
+    std::transform(mode.begin(), mode.end(), mode.begin(), [](unsigned char c) { return std::tolower(c); });
 
     // validate arguments
     if (simulation_steps <= 0) {
-        LOG(ERROR) << "'steps' must be greater than 0, was " << simulation_steps;
+        LOG(ERROR) << "'steps' must be greater than 0, was '" << simulation_steps << "'";
         return 1;
     }
     if (board_width <= 0) {
-        LOG(ERROR) << "'width' must be greater than 0, was " << board_width;
+        LOG(ERROR) << "'width' must be greater than 0, was '" << board_width << "'";
         return 1;
     }
     if (board_height <= 0) {
-        LOG(ERROR) << "'height' must be greater than 0, was " << board_height;
+        LOG(ERROR) << "'height' must be greater than 0, was '" << board_height << "'";
         return 1;
     }
-    if ((mode != "simple") | (mode != "advanced")) {
-        LOG(ERROR) << "'mode' must be 'simple' or 'advanced' " << mode;
+    if ((mode != "simple") && (mode != "advanced")) {
+        LOG(ERROR) << "'mode' must be 'simple' or 'advanced', was '" << mode << "'";
         return 1;
     }
 
@@ -95,17 +107,18 @@ int main(int argc, char **argv) {
             board_write->clear();
 
             Stopwatch stopwatch;
-            
-            BoardServerMPISimple server = BoardServerMPISimple(board_read, board_write, simulation_steps);
 
-            if (mode == "advanced")
-            {
+            if (mode == "advanced") {
                 BoardServerMPIAdvanced server = BoardServerMPIAdvanced(board_read, board_write, simulation_steps);
+                stopwatch.start();
+                server.start();
+                stopwatch.stop();
+            } else {
+                BoardServerMPISimple server = BoardServerMPISimple(board_read, board_write, simulation_steps);
+                stopwatch.start();
+                server.start();
+                stopwatch.stop();
             }
-
-            stopwatch.start();
-            server.start();
-            stopwatch.stop();
 
             if (output_path.length() > 0) {
                 board_read->exportAll(output_path);
@@ -119,15 +132,14 @@ int main(int argc, char **argv) {
             delete board_write;
         } else {
             // is client
-            
-            LifeClientMPISimple client = LifeClientMPISimple(server_rank);
 
-            if (mode == "advanced")
-            {
+            if (mode == "advanced") {
                 LifeClientMPIAdvanced client = LifeClientMPIAdvanced(server_rank);
+                client.start();
+            } else {
+                LifeClientMPISimple client = LifeClientMPISimple(server_rank);
+                client.start();
             }
-
-            client.start();
         }
     } catch (const std::exception &e) {
         // error handling here
